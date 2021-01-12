81ad2b5c674275de2831b8ca93becb9e272c7523
sitanshu4933 <62454843+sitanshu4933@users.noreply.github.com>
2021-01-12 14:57:27 +0530

    Delete dev.js

--- client/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURI:'mongodb+srv://sid:eHerwbKgn091829v@cluster0.kx9pm.mongodb.net/insta_clone?retryWrites=true&w=majority',
-    JWT_SECRET:'jwgwqndwqdhjdjogdhqnddjgfwen2vfifuwofyofjweb',
-    SENDGRID_API:"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",
-    EMAIL:"http://localhost:3000"
-}
